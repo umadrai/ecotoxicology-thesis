dd1dc5c4beeeaf02fe27d9f0f0cdc38c69597481
Ul Hassan Rai <uulhassa@nt.item.fraunhofer.de>
2021-08-19 12:51:07 +0200

    Using DT with Iris data. Tring groups with Shiny.

--- tutorial/groups.R
+++ tutorial/groups.R
@@ -2,25 +2,29 @@ library(shiny)
 library(plotly)
 library(DT)
 
+
+#ori <- datatable(orig)
+
 ui <- fluidPage(
   plotlyOutput('myPlot'),
   fluidRow(
-        DT::dataTableOutput("res")
+    DT::dataTableOutput("res")
   )
+  
+  
 )
 
 server <- function(input, output, session){
   output$myPlot = renderPlotly({
-    plot_ly(data = iris, x = ~Sepal.Length, y = ~Petal.Length, color = ~Species) %>%
+    plot_ly(data = orig, x = orig$DIM_1, y = orig$DIM_2, color = groups) %>%
       layout(dragmode = "select")
   })
   
   output$res <- renderDataTable({
     d <- event_data("plotly_selected")
-    a <- subset(iris, (iris$Sepal.Length %in% d$x & iris$Petal.Length %in% d$y))
+    a <- subset(orig, (orig$DIM_1 %in% d$x & orig$DIM_2 %in% d$y))
     a
   })
-  
 }
 
 shinyApp(ui, server)
